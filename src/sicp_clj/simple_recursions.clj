7c9aa02cd554602414b0daef38baae4b156dbcb9
Sergei <hentai.exe@gmail.com>
2023-03-13 00:01:14 +0300

    кус

---
+++ src/sicp_clj/simple_recursions.clj
@@ -0,0 +1,37 @@
+(ns sicp-clj.simple-recursions)
+
+;; this is recursive procedure
+(defn plus [a b]
+  (println "plus " a " " b)
+  (if (= a 0)
+    b
+    (inc (plus (dec a) b))))
+
+;; this is recursive process
+(defn other-plus [a b]
+  (println "other-plus " a " " b)
+  (if (= a 0)
+    b
+    (other-plus (dec a) (inc b))))
+
+(plus 5 6)
+(other-plus 5 6)
+
+;; очень быстро растёт, сволоч
+(defn ackerman [x y]
+  (cond (= y 0) 0
+        (= x 0) (* y 2)
+        (= y 1) 2
+        :else (ackerman (- x 1) (ackerman x (- y 1)))))
+
+(ackerman 3 2)
+
+;; классика!
+(defn fib [n]
+  {:pre [(>= n 0)]}
+  (cond (= n 0) 0
+        (= n 1) 1
+        :else (+ (fib (- n 1)) (fib (- n 2)))))
+
+(map fib (range 20))
+
